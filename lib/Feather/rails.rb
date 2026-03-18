9ddcc328e26b83e48132eeebc2ae32956bc066ff
Brandyn Britton <brandynbb96@gmail.com>
2026-03-18 19:34:56 +0800

    fix: remove weird tracking of upercase Feather?

--- lib/Feather/rails.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "rails/acts_as_sighting"
-require_relative "rails/railtie"
--- lib/Feather/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Feather
-  VERSION = "0.2.0"
-end
